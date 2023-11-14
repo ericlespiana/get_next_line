22ad0f522a4f9a0f4462b474e9d96f83a7029f57
Ericles Piana <pianericles@gmail.com>
2023-11-14 14:49:21 -0300

    done

---
+++ get_next_line.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erpiana <erpiana@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:02:57 by erpiana           #+#    #+#             */
+/*   Updated: 2023/11/14 14:12:51 by erpiana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+static char	*rest_file(char *file)
+{
+	size_t	i;
+	size_t	j;
+	char	*rest;
+
+	i = 0;
+	while (file[i] != '\n' && file[i])
+		i++;
+	if (!(file[i]))
+	{
+		free(file);
+		return (NULL);
+	}
+	if (file[i] == '\n')
+		i++;
+	rest = malloc(sizeof(char) * (ft_strlen(file) + 1 - i));
+	if (!rest)
+		return (NULL);
+	j = 0;
+	while (file[i] != '\0')
+		rest[j++] = file[i++];
+	rest[j] = '\0';
+	free(file);
+	return (rest);
+}
+
+static char	*cut_line(char *file)
+{
+	char	*line;
+	int		i;
+
+	i = 0;
+	if (!*file)
+		return (NULL);
+	while (file[i] != '\n' && file[i])
+		i++;
+	line = (char *)malloc(sizeof(char) * (i + 2));
+	if (!line)
+		return (NULL);
+	i = 0;
+	while (file[i] != '\n' && file[i])
+	{
+		line[i] = file[i];
+		i++;
+	}
+	if (file[i] == '\n')
+		line[i++] = '\n';
+	line[i] = '\0';
+	return (line);
+}
+
+static char	*read_file(char *line, int fd)
+{
+	int		flag;
+	char	*buffer;
+
+	flag = 1;
+	buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buffer)
+		return (NULL);
+	while (flag > 0 && !ft_strchr(line, '\n'))
+	{
+		flag = read(fd, buffer, BUFFER_SIZE);
+		if (flag < 0)
+		{
+			free(buffer);
+			return (NULL);
+		}
+		buffer[flag] = '\0';
+		line = ft_strjoin(line, buffer);
+	}
+	free(buffer);
+	return (line);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	*file;
+	char		*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	file = read_file(file, fd);
+	if (!file)
+		return (NULL);
+	line = cut_line(file);
+	file = rest_file(file);
+	return (line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: erpiana <erpiana@student.42.rio>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/14 14:03:36 by erpiana           #+#    #+#             */
+/*   Updated: 2023/11/14 14:15:11 by erpiana          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+char	*get_next_line(int fd);
+char	*ft_strdup(char const *s);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strjoin(char *s1, char *s2);
+size_t	ft_strlen(char const *str);
+
+#endif
